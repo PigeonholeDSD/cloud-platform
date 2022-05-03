4bf7be344cd0c92aa452d62d009f0e73e9229fa6
Magi_karp <magikaaarp@outlook.com>
2022-05-03 22:12:48 +0800

    test: add Test 9.Acquire the current version of the device model

--- test/9_test.py
+++ test/9_test.py
@@ -6,18 +6,20 @@
 
 import os
 import uuid
+import time
 import pytest
+import tarfile
+import random
 import requests
 from names import *
-import simdev
-from simdev import SimDevice 
+from simdev import *
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-def generate_url(id):
-    if id == None:
-        id = uuid.uuid4()
-    return API_BASE + "/device/" + str(id) + "/model"
+def generate_url(idd=None):
+    if idd == None:
+        idd = uuid.uuid4()
+    return API_BASE + "/device/" + str(idd) + "/model"
 
 def log_in_session() -> requests.Session:
     s = requests.Session()
@@ -25,12 +27,76 @@ def log_in_session() -> requests.Session:
     s.post(API_BASE + "/session", json=body)
     return s
     
-def test_good_getmodel():
+def test_good_get_base_model():
     simd = SimDevice()
     ts = requests.get(API_BASE + "/timestamp").text
     head = {"Authorization": simd.ticket(ts)}
     res = requests.get(generate_url(simd.id), headers=head)
     assert "Last-Modified" not in res.headers
+
+def generate_file(name: str):
+    with open(name, "w") as f:
+        for _ in range(10):
+            f.write(str(random.randint(1,1000000))+"\n")
+
+def generate_tgz(name: str):
+    n_file = random.randint(1,5)
+    with tarfile.open(name, "w:gz") as ftar:
+        for i in range(n_file):
+            fname = name[:-4] + str(i) + ".csv"
+            generate_file(fname)
+            ftar.add(fname)
+            os.remove(fname)
+
+def hash_tar(name: str):
+    hash_tar = hash_file(name)
+    os.remove(name)
+    return hash_tar
+
+def hash_content(content):
+    with open("_tmp", "wb") as f:
+        f.write(content)
+    return hash_tar("_tmp")
+
+def test_good_get_model():
+    simd = SimDevice()
+    s = log_in_session()
+    url = generate_url(simd.id)
+    generate_tgz("t1.tgz")
+    files = {"model": ("f1.tgz", open("t1.tgz", "rb"),\
+        "application/octet-stream")}
+    res = s.put(url, files=files)
+    h1 = hash_tar("t1.tgz")
+    
+    ts = requests.get(API_BASE + "/timestamp").text
+    head = {"Authorization": simd.ticket(ts)}
+    res = requests.get(generate_url(simd.id), headers=head)
+    print(res.headers["Last-Modified"])
+    print(time.strftime('%a, %d %b %Y %H', time.gmtime(time.time())))
+    assert res.headers["Last-Modified"].find(time.strftime(\
+        '%a, %d %b %Y %H', time.gmtime(time.time()))) == 0
+    assert hash_content(res.content) == h1
+    assert res.status_code == 200
+
+def test_signature():
+    simd = SimDevice()
+    s = log_in_session()
+    url = generate_url(simd.id)
+    generate_tgz("t1.tgz")
+    files = {"model": ("f1.tgz", open("t1.tgz", "rb"),\
+        "application/octet-stream")}
+    res = s.put(url, files=files)
+    h1 = hash_tar("t1.tgz")
+    
+    ts = requests.get(API_BASE + "/timestamp").text
+    head = {"Authorization": simd.ticket(ts)}
+    res = requests.get(generate_url(simd.id), headers=head)
+    print(res.headers["Last-Modified"])
+    print(time.strftime('%a, %d %b %Y %H', time.gmtime(time.time())))
+    assert res.headers["Last-Modified"].find(time.strftime(\
+        '%a, %d %b %Y %H', time.gmtime(time.time()))) == 0
+    assert hash_content(res.content) == h1
+    assert simd.verify(h1, res.headers["Signature"])
 
 if __name__ == "__main__":
     pytest.main(["./9_test.py"])
