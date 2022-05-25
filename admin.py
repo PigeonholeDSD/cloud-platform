8306f999f9bb1533eef57eb498dc699a24e0b895
AlexCSTech <i@ifan.dev>
2022-05-25 21:36:22 +0800

    update: remove /model/base

--- admin.py
+++ admin.py
@@ -5,7 +5,6 @@ from flask import Blueprint, request, session, jsonify
 import error
 from util import *
 import db.admin
-import db.model
 import db.device
 
 bp = Blueprint('admin', __name__)
@@ -31,18 +30,4 @@ def create_session():
 def session_delete():
     session.pop('user', None)
     session.pop('pass', None)
-    return '', 200
-
-
-@bp.put('/model/base')
-@check(admin_only=True)
-def put_model_base():
-    # admin_only()
-    file = request.files.get('model')
-    if not file:
-        raise error.APISyntaxError('No file uploaded')
-    path = os.path.join(mkdtemp(), 'model')
-    file.save(path)
-    db.model.setBase(path)
-    shutil.rmtree(os.path.dirname(path))
     return '', 200
