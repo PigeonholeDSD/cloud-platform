53e979f4120a6b99eb7d146ecd159027db558065
AlexCSTech <i@ifan.dev>
2022-05-29 19:22:28 +0800

    fix: add 2-level priority in /api/model/<algo>

--- model.py
+++ model.py
@@ -20,7 +20,10 @@ def get_model():
 def get_algo(algo: str):
     with open('algo/algo.json', 'r') as f:
         algo_list = json.load(f)
-    return send_file(open(algo_list[algo]['base']), 'application/octet-stream')
+    path = (db.device.get(uuid.UUID(int=0)).model[algo]
+        or algo_list[algo]['base'].replace('$ALGO', 'algo')
+    )
+    return send_file(open(path), 'application/octet-stream')
 
 @bp.put('model/<string:algo>')
 @check(admin_only=True)
