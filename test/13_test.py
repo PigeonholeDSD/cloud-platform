626a1da9c2289f56a26f8a91080687aad3e45a8a
Magi_karp <magikaaarp@outlook.com>
2022-05-30 18:15:57 +0800

    test: modify test 13

--- test/13_test.py
+++ test/13_test.py
@@ -11,6 +11,8 @@ import os
 import uuid
 import pytest
 import json
+import tarfile
+import random
 import requests
 from names import *
 from simdev import *
@@ -31,8 +33,9 @@ def log_in_session() -> requests.Session:
 def test_delete_straightforward():
     s = log_in_session()
     url = generate_url()
-    req = {"ban": True}
-    res = s.delete(url, json=req)
+    res = s.delete(url)
+    assert res.status_code == 200
+    res = s.delete(url)
     assert res.status_code == 200
 
 def test_good_delete():
@@ -50,34 +53,70 @@ def test_good_delete():
     assert json.loads(res.text) == body_email
     
     s = log_in_session()
-    req = {"ban": True}
-    res = s.delete(url, json=req)
+    res = s.delete(url)
     assert res.status_code == 200
     
     res = requests.get(url+"/email", headers=head)
     assert res.status_code == 404
 
-def test_bad_delete():
+def generate_file(name: str):
+    with open(name, "w") as f:
+        for _ in range(10):
+            f.write(str(random.randint(1,1000000))+"\n")
+            
+def generate_tgz(name: str):
+    n_file = random.randint(1,5)
+    with tarfile.open(name, "w:gz") as ftar:
+        for i in range(n_file):
+            fname = name[:-4] + str(i) + ".csv"
+            generate_file(fname)
+            ftar.add(fname)
+            os.remove(fname)
+
+def hash_tar(name: str):
+    hash_tar = hash_file(name)
+    os.remove(name)
+    return hash_tar
+
+def hash_content(content):
+    with open("_tmp", "wb") as f:
+        f.write(content)
+    return hash_tar("_tmp")
+
+def test_good_delete2():
     simd = SimDevice()
+    
+    s = log_in_session()
+    url = generate_url(simd.id) + "/model/"
+    generate_tgz("t1.tgz")
+    files = {"model": ("f1.tgz", open("t1.tgz", "rb"),\
+        "application/octet-stream")}
+    res = s.put(url + ALGO[0], files=files)
+    h1 = hash_tar("t1.tgz")
     
     ts = requests.get(API_BASE + "/timestamp").text
     head = {"Authorization": simd.ticket(ts)}
-    body_email = {"email": "pigeonholedevice@ciel.dev"}
-    url = generate_url(simd.id)
-    res = requests.post(url+"/email", json=body_email, headers=head)
+    res = requests.get(url + ALGO[0], headers=head)
+    assert res.status_code == 200
+    assert hash_content(res.content) == h1
+    assert "Last-Modified" in res.headers
+    
+    res = requests.get(url + ALGO[1], headers=head)
+    assert res.status_code == 200
+    assert "Last-Modified" not in res.headers
+    
+    res = requests.get(url + ALGO[0][1:], headers=head)
+    assert res.status_code == 404
+    
+    res = requests.delete(generate_url(simd.id), headers=head)
     assert res.status_code == 200
     
-    res = requests.get(url+"/email", headers=head)
-    assert res.status_code == 200
-    assert json.loads(res.text) == body_email
+    res = requests.get(generate_url(simd.id) + "/email", headers=head)
+    assert res.status_code == 404
     
-    s = log_in_session()
-    req = {"ban": 123}
-    res = s.delete(url, json=req)
-    assert res.status_code == 400
-    
-    res = requests.get(url+"/email", headers=head)
+    res = requests.get(url + ALGO[0], headers=head)
     assert res.status_code == 200
+    assert "Last-Modified" not in res.headers
 
 if __name__ == "__main__":
     pytest.main(["./13_test.py"])
